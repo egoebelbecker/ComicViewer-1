7dda184546bd0f1b7f9296b284b1620e2e120a65
egoebelbecker <eric@furrybytes.com>
2015-07-19 17:58:22 -0400

    Catch Exception
    Catch Archive exception, display a message box.

--- ComicViewer/MainWindow.xaml.cs
+++ ComicViewer/MainWindow.xaml.cs
@@ -63,6 +63,8 @@ namespace ComicViewer
             catch (InvalidOperationException ioe)
             {
                 Console.WriteLine("Error opening file " + ioe.Message);
+                MessageBox.Show("Error opening file. Please try another.");
+
             }
         }
 
